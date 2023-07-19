08dcadbc4b216590bb63966ae8eb5e33ea6644f1
SPN <10730985+splesanaden@users.noreply.github.com>
2023-07-19 13:01:47 -0600

    1

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@ import { RouterLink } from "vue-router";
       class="header-nav--wrapper | grid grid-cols-2 gap-4 grid-flow-row | w-screen h-full xl:max-w-screen-2xl 2xl:max-w-screen-2xl 3xl:max-w-screen-3xl | items-stretch | 2xl:mx-auto | ps-8 xl:pe-0 3xl:px-16"
     >
       <div class="grid items-center">
-        <img src="images/Louis Keene Logo@2x.svg" alt="Louis Keene Logo" />
+        <img src="/images/Louis Keene Logo@2x.svg" alt="Louis Keene Logo" />
       </div>
 
       <div class="xl:grid hidden items-stretch justify-end">
